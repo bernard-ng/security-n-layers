4fd4602f61e59d68f4eacf8974a9dcb8e6286c64
bernard-ng <ngandubernard@gmail.com>
2020-02-18 20:19:19 +0100

    feat(auth): add login authentication

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class HomeController
+ * @package App\Controller
+ * @author bernard-ng <ngandubernard@gmail.com>
+ */
+class HomeController extends AbstractController
+{
+
+    /**
+     * @Route(path="", name="app_home", methods={"GET"})
+     * @return Response
+     * @author bernard-ng <ngandubernard@gmail.com>
+     */
+    public function index(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+}
---
+++ src/Controller/Security/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller\Security;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+/**
+ * Class LoginController
+ * @package App\Controller
+ * @author bernard-ng <ngandubernard@gmail.com>
+ */
+class LoginController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login", methods={"GET", "POST"})
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     */
+    public function logout()
+    {
+        throw new \LogicException(
+            'This method can be blank - it will be intercepted by the logout key on your firewall.'
+        );
+    }
+}
